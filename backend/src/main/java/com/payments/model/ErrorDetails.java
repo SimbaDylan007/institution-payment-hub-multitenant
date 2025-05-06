8b0ddb6f46b82c9b283a93a104ff9f25794fc661
smachangachanga <machangachangad@gmail.com>
2025-05-06 18:53:10 +0200

    with manage students

--- backend/src/main/java/com/payments/model/ErrorDetails.java
+++ backend/src/main/java/com/payments/model/ErrorDetails.java
@@ -1,3 +1,14 @@
+//
+//package com.payments.model;
+//
+//import lombok.Data;
+//
+//@Data
+//public class ErrorDetails {
+//    private String details;
+//    private String message;
+//    private String timestamp;
+//}
 
 package com.payments.model;
 
@@ -8,4 +19,15 @@ public class ErrorDetails {
     private String details;
     private String message;
     private String timestamp;
+
+    // No-arg constructor
+    public ErrorDetails() {
+    }
+
+    // All-args constructor
+    public ErrorDetails(String message, String timestamp, String details) {
+        this.message = message;
+        this.timestamp = timestamp;
+        this.details = details;
+    }
 }
